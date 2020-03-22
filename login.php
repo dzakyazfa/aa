2036de700cf9952c4e7e2c701b27513df6d9960e
unknown <dzaky_azfa_28rpl@student.smktelkom-mlg.sch.id>
2020-03-23 01:17:52 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>LOGIN</title>
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="log.css">
+ </head>
+ <body><div id="card"> 
+  <div id="card-content">
+  <div id="card-title">
+    <h2>LOGIN</h2>
+    <div class="underline-title"></div>
+  </div>
+</div>
+<form action="logen.php" method="post" class="form"> <!-- <label for="user-id" style="padding-top:22px">&nbsp;ID</label>
+  <input
+   id="user-id"
+   class="form-content"
+   type="id"
+   name="id"
+   autocomplete="on"
+   required />
+  <div class="form-border"></div> -->
+  <label for="user-email" style="padding-top:22px">&nbsp;Username</label>
+  <input
+  id="user-username"
+   class="form-content"
+   type="text"
+   name="username"
+   required />
+  <div class="form-border"></div>
+  <label for="user-password" style="padding-top:22px">&nbsp;Password</label>
+  <input
+   id="user-password"
+   class="form-content"
+   type="password"
+   name="password"
+   required />
+  <div class="form-border"></div>
+<input id="submit-btn" type="submit" name="submit" value="LOGIN" />
+</form>
+ </div>
+ </body>
+</html>
